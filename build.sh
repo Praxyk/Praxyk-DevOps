1cad742c7fdd05db7e3cd0b83559a347ecf2a25c
jhallard <john@jhallard.com>
2015-11-06 21:25:20 -0800

    Added fake auth info for devops branch

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@
 # this script installs and builds all system wide (ubuntu) and python level
 # requirements needed by the devops server
 
+export DEBIAN_FRONTEND=noninteractive
+
 xargs sudo apt-get -y install < .ubuntu_install
 
 RETVAL=$?
@@ -16,6 +18,15 @@ sudo pip install -r .pip_install
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo "  Devops : Pip Requirements Install Success"
 [ $RETVAL -ne 0 ] && echo "  Devops : Pip Requirements Install Failure" && exit 1
+
+sudo apt-get -q -y install mysql-server
+sudo service mysql restart
+mysql -e "create database IF NOT EXISTS test;" -uroot
+
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo "  DevOps : MySQL Requirements Install Success"
+[ $RETVAL -ne 0 ] && echo "  DevOps : MySQL Requirements Install Failure" && exit 1
+
 
 exit 0
 
